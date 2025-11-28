9f38108b77148390834608f6ca7bc8892e2c1429
minjee <kimmj1106@naver.com>
2025-11-28 15:50:21 +0900

    [MOD] POST /user : race condition 방어 로직 추가

--- src/main/java/com/example/project/user/service/UserServiceImpl.java
+++ src/main/java/com/example/project/user/service/UserServiceImpl.java
@@ -7,6 +7,7 @@ import com.example.project.user.dto.response.UserJoinRes;
 import com.example.project.user.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,18 +24,24 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserJoinRes userJoin(UserJoinReq req) {
 
-        // 1. 이메일 중복 체크
+        //1. 이메일 중복 체크
         int count = userMapper.existsByEmail(req.getEmail());
         if (count > 0) {
-            throw new BusinessException(ErrorCode.USER_EMAIL_DUPLICATED);
+            throw new BusinessException(ErrorCode.USER_EMAIL_DUPLICATED); //409
         }
 
-        // 2. 비밀번호 암호화
+        //2. 비밀번호 암호화
         String encodedPw = passwordEncoder.encode(req.getPassword());
         req.setPassword(encodedPw);
 
-        //4. db 저장
-        userMapper.insertUser(req);
+        //3. db 저장
+        try {
+            userMapper.insertUser(req);
+        } catch (DuplicateKeyException e) {
+            // // race condition 등으로 인해 동시에 insert 시도된 경우
+            log.warn("Duplicate email detected on insert. email={}", req.getEmail(), e);
+            throw new BusinessException(ErrorCode.USER_EMAIL_DUPLICATED); //409
+        }
 
         return UserJoinRes.of(req);
     }
